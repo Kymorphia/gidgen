b443a8876748834df3e493f4055ef533a7002053
Dejan Lekic <dejanlekic@bmlltech.com>
2025-02-28 21:57:14 +0000

    Removed the comment.

--- source/gir/repo.d
+++ source/gir/repo.d
@@ -854,7 +854,7 @@ final class Repo : Base
   {
     auto writer = new CodeWriter(path);
 
-    writer ~= ["module " ~ namespace.toLower ~ ".global; // DEJAN C", ""];
+    writer ~= ["module " ~ namespace.toLower ~ ".global;", ""];
 
     // Create the function writers first to construct the imports
     defs.beginImports(globalStruct);
