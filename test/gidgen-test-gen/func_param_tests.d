f933b1cbb56e7c80cab2638cc562a99248adb2c1
Element Green <element@kymorphia.com>
2026-02-21 10:17:27 -0600

    Integration test improvements * Separate func param tests into indvidual methods * Remove -t 1 from silly dub test command which was a work around for multi-thread related crashes in the giD loader when the libgidgen-test.so file could not be found (now fixed in giD)

--- test/gidgen-test-gen/func_param_tests.d
+++ test/gidgen-test-gen/func_param_tests.d
@@ -21,158 +21,10 @@ void genFuncParamsTests()
       case Unknown:
         break;
       case Basic:
-        genInstance.dCow ~= ["", "@(\"FuncBasicParams\")", "unittest", "{"];
-
-        dstring type, cType, dFuncName, funcName, cFuncName, decl;
-
-        static foreach (i, basic; BasicTypes)
-        { // Basic input -> return parameter tests
-          type = basic.stringof.to!dstring;
-          cType = gBasicTypes[i].to!dstring;
-          dFuncName = "func"d ~ type.capitalize ~ "InReturn"d;
-          funcName = "func_"d ~ type ~ "_in_return"d;
-          cFuncName = genInstance.cSymPrefix.to!dstring ~ "_"d ~ funcName;
-          decl = cType ~ " "d ~ cFuncName ~ "("d ~ cType ~ " param)"d;
-
-          genInstance.cCow ~= ["", decl.to!dstring, "{", "return param;", "}"];
-
-          writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-            retType: cType.to!string, cRetType: cType.to!string,
-            params: [GirParam(name: "param", type: cType.to!string, cType: cType.to!string)]));
-
-          static if (isFloatingPoint!basic)
-            genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ") == cast(" ~ type ~ ")-"
-              ~ basic.max.to!dstring ~ ");";
-          else
-            genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ") == cast(" ~ type ~ ")"
-              ~ basic.min.to!dstring ~ ");";
-
-          genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ") == cast(" ~ type ~ ")"
-            ~ basic.max.to!dstring ~ ");";
-
-          // Basic input -> output parameter tests
-          dFuncName = "func" ~ type.capitalize ~ "InToOut";
-          funcName = "func_"d ~ type ~ "_in_to_out";
-          cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-          decl = "void " ~ cFuncName ~ "(" ~ cType ~ " param, " ~ cType ~ "* out_param)";
-
-          genInstance.cCow ~= ["", decl.to!dstring, "{", "*out_param = param;", "}"];
-
-          writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-            params: [
-              GirParam(name: "param", type: cType.to!string, cType: cType.to!string),
-              GirParam(name: "out_param", type: cType.to!string, cType: cType.to!string ~ "*",
-                direction: ParamDirection.Out)
-            ]));
-
-          genInstance.dCow ~= ["{"d, type ~ " outVal;"];
-
-          static if (isFloatingPoint!basic)
-            genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ", outVal);",
-              "assert(outVal == cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ");"];
-          else
-            genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ", outVal);",
-              "assert(outVal == cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ");"];
-
-          genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ", outVal);",
-            "assert(outVal == cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ");"];
-
-          genInstance.dCow ~= "}"d;
-
-          // Basic inout parameter tests
-          dFuncName = "func" ~ type.capitalize ~ "Inout";
-          funcName = "func_"d ~ type ~ "_inout";
-          cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-          decl = "void " ~ cFuncName ~ "(" ~ cType ~ "* param)";
-
-          genInstance.cCow ~= ["", decl.to!dstring, "{", "*param = !*param;", "}"];
-
-          writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-            params: [
-              GirParam(name: "param", type: cType.to!string, cType: cType.to!string ~ "*",
-                direction: ParamDirection.InOut)
-            ]));
-
-          genInstance.dCow ~= ["{"d, type ~ " val = cast(" ~ type ~ ")0;", dFuncName ~ "(val);",
-            "assert(val == cast(" ~ type ~ ")!0);", "}"d];
-        }
-
-        genInstance.dCow ~= "}";
+        genFuncBasicParamTests;
         break;
       case String:
-        genInstance.dCow ~= ["", "@(\"FuncStringParams\")", "unittest", "{"];
-
-        // String in -> return no transfer
-        auto dFuncName = "funcStringInReturnTransNone"d;
-        auto funcName = "func_string_in_return_trans_none"d;
-        auto cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-        auto decl = "const char* " ~ cFuncName ~ "(const char* param)";
-
-        genInstance.cCow ~= ["", decl.to!dstring, "{", "return param;", "}"];
-
-        writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-          retType: "utf8", cRetType: "const char *", retOwnership: Ownership.None,
-          params: [GirParam(name: "param", type: "utf8", cType: "const char *", ownership: Ownership.None)]));
-
-        // memCapture
-        genInstance.dCow ~= "assert("d ~ dFuncName ~ "(\"blah123\") == \"blah123\");"d;
-        // memAssertNone
-
-        // String in -> return both transfer full
-        dFuncName = "funcStringInReturnTransFull"d;
-        funcName = "func_string_in_return_trans_full"d;
-        cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-        decl = "char* " ~ cFuncName ~ "(char* param)";
-
-        genInstance.cCow ~= ["", decl.to!dstring, "{", "char* ret = g_strdup(param);", "g_free(param);", "return ret;", "}"];
-
-        writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-          retType: "utf8", cRetType: "char *", retOwnership: Ownership.Full,
-          params: [GirParam(name: "param", type: "utf8", cType: "char *", ownership: Ownership.Full)]));
-
-        // memCapture
-        genInstance.dCow ~= "assert(" ~ dFuncName ~ "(\"blah123\") == \"blah123\");";
-        // memAssertAllocFree
-
-        // String in -> out no transfer
-        dFuncName = "funcStringInToOutTransNone"d;
-        funcName = "func_string_in_to_out_trans_none"d;
-        cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-        decl = "void " ~ cFuncName ~ "(const char* param, const char** outParam)";
-
-        genInstance.cCow ~= ["", decl.to!dstring, "{", "*outParam = param;", "}"];
-
-        writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-          params: [
-            GirParam(name: "param", type: "utf8", cType: "const char *", ownership: Ownership.None),
-            GirParam(name: "out_param", type: "utf8", cType: "const char **", direction: ParamDirection.Out,
-              ownership: Ownership.None)
-          ]));
-
-        // memCapture
-        genInstance.dCow ~= ["{", "string outVal;", dFuncName ~ "(\"blah123\", outVal);", "assert(outVal == \"blah123\");", "}"];
-        // memAssertNone
-
-        // String in -> out both transfer full
-        dFuncName = "funcStringInToOutTransFull"d;
-        funcName = "func_string_in_to_out_trans_full"d;
-        cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
-        decl = "void " ~ cFuncName ~ "(char* param, char** out_param)";
-
-        genInstance.cCow ~= ["", decl.to!dstring, "{", "*out_param = g_strdup(param);", "g_free(param);", "}"];
-
-        writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
-          params: [
-            GirParam(name: "param", type: "utf8", cType: "char *", ownership: Ownership.Full),
-            GirParam(name: "out_param", type: "utf8", cType: "char **", direction: ParamDirection.Out,
-              ownership: Ownership.Full)
-          ]));
-
-        // memCapture
-        genInstance.dCow ~= ["{", "string outVal;", dFuncName ~ "(\"blah123\", outVal);", "assert(outVal == \"blah123\");", "}"];
-        // memAssertAllocFree
-
-        genInstance.dCow ~= "}";
+        genFuncStringParamTests;
         break;
       case BasicAlias:
         break;
@@ -206,6 +58,164 @@ void genFuncParamsTests()
         break;
     }
   }
+}
+
+private void genFuncBasicParamTests()
+{
+  genInstance.dCow ~= ["", "@(\"FuncBasicParams\")", "unittest", "{"];
+
+  dstring type, cType, dFuncName, funcName, cFuncName, decl;
+
+  static foreach (i, basic; BasicTypes)
+  { // Basic input -> return parameter tests
+    type = basic.stringof.to!dstring;
+    cType = gBasicTypes[i].to!dstring;
+    dFuncName = "func"d ~ type.capitalize ~ "InReturn"d;
+    funcName = "func_"d ~ type ~ "_in_return"d;
+    cFuncName = genInstance.cSymPrefix.to!dstring ~ "_"d ~ funcName;
+    decl = cType ~ " "d ~ cFuncName ~ "("d ~ cType ~ " param)"d;
+
+    genInstance.cCow ~= ["", decl.to!dstring, "{", "return param;", "}"];
+
+    writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+      retType: cType.to!string, cRetType: cType.to!string,
+      params: [GirParam(name: "param", type: cType.to!string, cType: cType.to!string)]));
+
+    static if (isFloatingPoint!basic)
+      genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ") == cast(" ~ type ~ ")-"
+        ~ basic.max.to!dstring ~ ");";
+    else
+      genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ") == cast(" ~ type ~ ")"
+        ~ basic.min.to!dstring ~ ");";
+
+    genInstance.dCow ~= "assert(" ~ dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ") == cast(" ~ type ~ ")"
+      ~ basic.max.to!dstring ~ ");";
+
+    // Basic input -> output parameter tests
+    dFuncName = "func" ~ type.capitalize ~ "InToOut";
+    funcName = "func_"d ~ type ~ "_in_to_out";
+    cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+    decl = "void " ~ cFuncName ~ "(" ~ cType ~ " param, " ~ cType ~ "* out_param)";
+
+    genInstance.cCow ~= ["", decl.to!dstring, "{", "*out_param = param;", "}"];
+
+    writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+      params: [
+        GirParam(name: "param", type: cType.to!string, cType: cType.to!string),
+        GirParam(name: "out_param", type: cType.to!string, cType: cType.to!string ~ "*",
+          direction: ParamDirection.Out)
+      ]));
+
+    genInstance.dCow ~= ["{"d, type ~ " outVal;"];
+
+    static if (isFloatingPoint!basic)
+      genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ", outVal);",
+        "assert(outVal == cast(" ~ type ~ ")-" ~ basic.max.to!dstring ~ ");"];
+    else
+      genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ", outVal);",
+        "assert(outVal == cast(" ~ type ~ ")" ~ basic.min.to!dstring ~ ");"];
+
+    genInstance.dCow ~= [dFuncName ~ "(cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ", outVal);",
+      "assert(outVal == cast(" ~ type ~ ")" ~ basic.max.to!dstring ~ ");"];
+
+    genInstance.dCow ~= "}"d;
+
+    // Basic inout parameter tests
+    dFuncName = "func" ~ type.capitalize ~ "Inout";
+    funcName = "func_"d ~ type ~ "_inout";
+    cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+    decl = "void " ~ cFuncName ~ "(" ~ cType ~ "* param)";
+
+    genInstance.cCow ~= ["", decl.to!dstring, "{", "*param = !*param;", "}"];
+
+    writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+      params: [
+        GirParam(name: "param", type: cType.to!string, cType: cType.to!string ~ "*",
+          direction: ParamDirection.InOut)
+      ]));
+
+    genInstance.dCow ~= ["{"d, type ~ " val = cast(" ~ type ~ ")0;", dFuncName ~ "(val);",
+      "assert(val == cast(" ~ type ~ ")!0);", "}"d];
+  }
+
+  genInstance.dCow ~= "}";
+}
+
+private void genFuncStringParamTests()
+{
+  genInstance.dCow ~= ["", "@(\"FuncStringParams\")", "unittest", "{"];
+
+  // String in -> return no transfer
+  auto dFuncName = "funcStringInReturnTransNone"d;
+  auto funcName = "func_string_in_return_trans_none"d;
+  auto cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+  auto decl = "const char* " ~ cFuncName ~ "(const char* param)";
+
+  genInstance.cCow ~= ["", decl.to!dstring, "{", "return param;", "}"];
+
+  writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+    retType: "utf8", cRetType: "const char *", retOwnership: Ownership.None,
+    params: [GirParam(name: "param", type: "utf8", cType: "const char *", ownership: Ownership.None)]));
+
+  // memCapture
+  genInstance.dCow ~= "assert("d ~ dFuncName ~ "(\"blah123\") == \"blah123\");"d;
+  // memAssertNone
+
+  // String in -> return both transfer full
+  dFuncName = "funcStringInReturnTransFull"d;
+  funcName = "func_string_in_return_trans_full"d;
+  cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+  decl = "char* " ~ cFuncName ~ "(char* param)";
+
+  genInstance.cCow ~= ["", decl.to!dstring, "{", "char* ret = g_strdup(param);", "g_free(param);", "return ret;", "}"];
+
+  writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+    retType: "utf8", cRetType: "char *", retOwnership: Ownership.Full,
+    params: [GirParam(name: "param", type: "utf8", cType: "char *", ownership: Ownership.Full)]));
+
+  // memCapture
+  genInstance.dCow ~= "assert(" ~ dFuncName ~ "(\"blah123\") == \"blah123\");";
+  // memAssertAllocFree
+
+  // String in -> out no transfer
+  dFuncName = "funcStringInToOutTransNone"d;
+  funcName = "func_string_in_to_out_trans_none"d;
+  cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+  decl = "void " ~ cFuncName ~ "(const char* param, const char** outParam)";
+
+  genInstance.cCow ~= ["", decl.to!dstring, "{", "*outParam = param;", "}"];
+
+  writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+    params: [
+      GirParam(name: "param", type: "utf8", cType: "const char *", ownership: Ownership.None),
+      GirParam(name: "out_param", type: "utf8", cType: "const char **", direction: ParamDirection.Out,
+        ownership: Ownership.None)
+    ]));
+
+  // memCapture
+  genInstance.dCow ~= ["{", "string outVal;", dFuncName ~ "(\"blah123\", outVal);", "assert(outVal == \"blah123\");", "}"];
+  // memAssertNone
+
+  // String in -> out both transfer full
+  dFuncName = "funcStringInToOutTransFull"d;
+  funcName = "func_string_in_to_out_trans_full"d;
+  cFuncName = genInstance.cSymPrefix.to!dstring ~ "_" ~ funcName;
+  decl = "void " ~ cFuncName ~ "(char* param, char** out_param)";
+
+  genInstance.cCow ~= ["", decl.to!dstring, "{", "*out_param = g_strdup(param);", "g_free(param);", "}"];
+
+  writeGirFunction(GirFunc(name: funcName.to!string, cName: cFuncName.to!string, funcType: FuncType.Function,
+    params: [
+      GirParam(name: "param", type: "utf8", cType: "char *", ownership: Ownership.Full),
+      GirParam(name: "out_param", type: "utf8", cType: "char **", direction: ParamDirection.Out,
+        ownership: Ownership.Full)
+    ]));
+
+  // memCapture
+  genInstance.dCow ~= ["{", "string outVal;", dFuncName ~ "(\"blah123\", outVal);", "assert(outVal == \"blah123\");", "}"];
+  // memAssertAllocFree
+
+  genInstance.dCow ~= "}";
 }
 
 private void writeGirFunction(GirFunc girFunc)
